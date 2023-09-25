86091c85460032666063c6af31723b25db1d69d0
Pavel Zakharov <trade93@gmail.com>
2023-09-24 20:16:47 -0500

    Render Pizzas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
+import pizzas from './assets/pizzas.json';
+
 function App() {
   return (
     <div className="wrapper">
@@ -18,8 +20,9 @@ function App() {
               </div>
               <h2 className="content__title">Все пиццы</h2>
               <div className="content__items">
-                 <PizzaBlock title='Чизбургер-пицца' price={395}/>
-                 <PizzaBlock title='Мексиканская' price={500}/>
+                 { pizzas.map((pizza) => (
+                  <PizzaBlock {...pizza}/>
+                 ))}
               </div>
             </div>
           </div>
